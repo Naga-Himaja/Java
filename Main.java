9dc1a3e162c7f57abe7d2438f740f1728b11da3c
K NAGA HIMAJA <himajakeela18@gmail.com>
2022-03-30 22:09:31 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,30 +1,21 @@
-package shape;
-import java.util.Scanner;
+import java.util.*;
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("1.Trianle\n2.Circle\n3.Rectangle\n4.Exit");
-		System.out.print("Enter your choice : ");
+	public static void main(String args[])
+	{
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number of elements : ");
 		int n = sc.nextInt();
-		while (n<4)
-		{
-			switch (n)
-			{
-			case 1 : System.out.print("Enter the dimensions of the triangle : ");
-					 Triangle t = new Triangle(sc.nextDouble(),sc.nextDouble());
-					 t.area(); break;
-			case 2 : System.out.print("Enter the radius of the circle : ");
-					 Circle c = new Circle(sc.nextDouble());
-					 c.area(); break;
-			case 3 : System.out.print("Enter the dimensions of the rectangle : ");
-					 Rectangle r = new Rectangle(sc.nextDouble(), sc.nextDouble());
-					 r.area(); break;
-			}
-			System.out.println("1.Triangle\n2.Circle\n3.Rectangle\n4.Exit");
-			System.out.println("Enter your choice : ");
-			n = sc.nextInt();
-		}
+		int arr[] = new int[n];
+		System.out.println("Enter "+n+" elements : ");
+		for(int i=0;i<n;i++)
+			arr[i] = sc.nextInt();
+		long start = System.nanoTime();
+		MergeSort.splitAndMerge(arr,n,0,n-1);
+		long end = System.nanoTime();
+		System.out.println("Sorted Array is : ");
+		MergeSort.display(arr,n);
+		System.out.print("Time taken for execution in Nano seconds : ");
+		System.out.println(end-start);
+		sc.close();
 	}
-
 }
